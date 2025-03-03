ef67db6541c784b07871945f975afab00af48723
bgoncharuck <dariydragon26@gmail.com>
2025-03-03 15:42:12 +0200

    chore: initial template

---
+++ android/app/src/main/kotlin/com/lbzsunalarm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lbzsunalarm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+Future<void> main() async {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(context) =>
+      const Center(child: Text('test', textDirection: TextDirection.ltr));
+}
